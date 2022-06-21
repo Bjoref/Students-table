f7b33121782a23e04be1624d9fdf312114b09cec
Bjoref <flamros@yandex.ru>
2022-06-21 21:06:08 +0300

    form v1.0

--- src/js/section-form/__button/section-form__button.js
+++ src/js/section-form/__button/section-form__button.js
@@ -1,13 +1,25 @@
 (() => {
     const showButton = document.querySelector('.section-form__button');
+    const submitButton = document.querySelector('.section-form__submit-button');
     const form = document.querySelector('.section-form__form');
+    const errorField = document.querySelector('.section-form__button-error');
 
     const showForm = () => {
         form.classList.remove('d-none');
         showButton.classList.add('d-none');
-        document.querySelector('.section-form__submit-button').disabled = true;
         document.querySelector('.section-form__submit-button').classList.add('disabled');
     };
 
     showButton.addEventListener('click', showForm);
+
+    submitButton.addEventListener('mouseover', (element) => {
+        if(element.target.classList.contains('disabled')) {
+            errorField.classList.remove('d-none')
+        };
+    });
+    submitButton.addEventListener('mouseout', (element) => {
+        if(element.target.classList.contains('disabled')) {
+            errorField.classList.add('d-none')
+        };
+    });
 })();
